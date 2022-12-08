511e1cfe46102ee2772d28f6db5d29d7d17bc671
Kevyn SOLA <kevyn.sola@gmail.com>
2022-12-08 16:41:49 +0100

    add style to chart and autocomplete

--- src/Controller/ChartController.php
+++ src/Controller/ChartController.php
@@ -13,16 +13,24 @@ class ChartController extends AbstractController
     #[Route('/chartjs', methods: ['GET'], name: 'app_chartjs')]
     public function showChart(ChartBuilderInterface $chartBuilder): Response
     {
-        $chart = $chartBuilder->createChart(Chart::TYPE_LINE); //Différent type de Chart à voir sur la Doc!
+        $chart = $chartBuilder->createChart(Chart::TYPE_POLAR_AREA); //Différent type de Chart à voir sur la Doc!
 
         $chart->setData([
-            'labels' => ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            'labels' => ['HTML', 'CSS', 'JS', 'SCSS', 'Twig', 'PHP', 'Bootstrap', 'Symfony'],
             'datasets' => [
                 [
-                    'label' => 'My First dataset',
-                    'backgroundColor' => 'rgb(255, 99, 132)',
-                    'borderColor' => 'rgb(255, 99, 132)',
-                    'data' => [0, 10, 5, 2, 20, 30, 45],
+                    'label' => 'Mon niveau',
+                    'data' => [18, 15, 5, 13, 18, 19, 17, 15],
+                    'backgroundColor' => [
+                        'rgb(0, 66, 44)',
+                        'rgb(66, 135, 245)',
+                        'rgb(69, 199, 26)',
+                        'rgb(164, 26, 199)',
+                        'rgb(199, 26, 35)',
+                        'rgb(212, 203, 32)',
+                        'rgb(184, 105, 20)',
+                        'rgb(20, 173, 184)',
+                    ],
                 ],
             ],
         ]);
